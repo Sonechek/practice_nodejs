c7552ffff803081659b8c69aa87b1e3b7096a4cb
Sonechek <71063702+Sonechek@users.noreply.github.com>
2022-10-10 15:03:26 +0500

    main

--- practice_4/program.js
+++ practice_4/program.js
@@ -1,5 +1,7 @@
-const {sortByLengthAndName, new_array_in_object, two_arrays_in_array} = require('./module')
+const {sortByLengthAndName, new_array_in_object, two_arrays_in_array, south_ball, Kungur_clients} = require('./module')
 
 sortByLengthAndName()
 new_array_in_object()
 two_arrays_in_array()
+south_ball()
+Kungur_clients
