69b092e6de57e48c9fdc3f4c5c3920230e3ab9f7
Sonechek <71063702+Sonechek@users.noreply.github.com>
2022-10-10 14:49:44 +0500

    main

--- practice_4/module.js
+++ practice_4/module.js
@@ -28,15 +28,19 @@ function new_array_in_object(){
 
 
 function two_arrays_in_array(){
-    console.log(
-        _(data.colors)
-            .zip(argb)
-            .map(arr => _.zipObject(['color','hex'], arr))
-            .sortBy(obj => obj.color)
-            .value()
-    )
+console.log(
+    _(data.argb)
+        .map(elm => '#' + elm.slice(0,3).map(num => num.toString(16)).join(''))
+        .zip(data.colors)
+        .map(arr => _.zipObject(['hex','color'],arr))
+        .sortBy(obj => obj.color)
+        .value()
+)
 }
 
+function south_ball(){
+    _
+}
 
 
 module.exports = {
