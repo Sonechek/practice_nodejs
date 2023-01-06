6f216efdc218694d04d2c9aff90f98c1217ee140
Alexander Shipulin <flex.tlex@gmail.com>
2023-01-06 22:24:37 +0500

    main

--- practice_7/app.js
+++ practice_7/app.js
@@ -1,6 +1,7 @@
 const http = require("http")
-const { about, get_files_filter} = require('./module')
+const { about, get_files_filter, cloneDeep} = require('./module')
 let { WorkData } = require('./library')
+
 
 
 const server = http.createServer()
@@ -10,23 +11,22 @@ let select_case = (args, response) => {
     try {
         switch (args.length) {
             case 1: // http://localhost:3000
-                about(response);
-                break;
+                about(response)
+                break
 
             case 2: // http://localhost:3000/json или http://localhost:3000/csv
                 let files =  get_files_filter('./timefall', args[1])
                 response.write(files.join('\n'))
-                break;
+                break
 
             case 3: // http://localhost:3000/json/users.json
-                let wd = new WorkData(`./timefall/${args[1]}/${args[2]}`);
-
+                let wd = new WorkData(`./timefall/${args[1]}/${args[2]}`)
                 console.log(wd)
-                response.write(JSON.stringify(wd.json, null, 4));
-                break;
+                response.write(JSON.stringify(wd.json, null, 4))
+                break
 
             case 4: // http://localhost:3000/json/users.json/?rating=desc&name=asc
-                let wd_sort = new WorkData(`./timefall/${args[1]}/${args[2]}`);
+                let wd_sort = new WorkData(`./timefall/${args[1]}/${args[2]}`)
                 let temp = args[3]
                     .toString()
                     .slice(1)
@@ -48,31 +48,30 @@ let select_case = (args, response) => {
                 directs = directs
                     .slice(0, -1)
                     .split(" ")
-
-                console.log(fields)
-                console.log(directs)
-
-                response.write(JSON.stringify(wd_sort.json, null, 4));
-                break;
+                let tempo_sort = cloneDeep(wd_sort)
+                // console.log(tempo_sort)
+                wd_sort.orderBy(fields,directs)
+                response.write(JSON.stringify(wd_sort.json, null, 4))
+                break
             default:
-                console.log(404)
-                response.statusCode = 404;
-                response.write('Запрос ошибочный...');
-                break;
+                console.log('404')
+                response.statusCode = 404
+                response.write('Запрос ошибочный...')
+                break
         }
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
-    response.end();
+    response.end()
 }
 
 let callback = (request, response) => {
-    let args = request.url.split('/');
-    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    if (args[args.length-1] === '') args.pop();
-    select_case(args, response);
+    let args = request.url.split('/')
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    if (args[args.length-1] === '') args.pop()
+    select_case(args, response)
 }
 
-server.on("request", callback);
+server.on("request", callback)
 
-server.listen(port, () => console.log(`localhost:${port}`));
+server.listen(port, () => console.log(`localhost:${port}`))
--- practice_7/module.js
+++ practice_7/module.js
@@ -1,5 +1,5 @@
 const about = (res) => {
-    let about = require('./timefall/json/about.json');
+    let about = require('./about.json');
     res.write(JSON.stringify(about, null, 4));
 }
 
@@ -24,6 +24,7 @@ const get_files_filter = (dir, ext) => {
     rec(dir);
     return name_files;
 }
+
 comparator = (a, b, fs, ds) => {
     if ((fs.length > 1) && (a[fs[0]] === b[fs[0]])) {
         return comparator(a, b, fs.slice(1,), ds.slice(1,));
@@ -31,10 +32,22 @@ comparator = (a, b, fs, ds) => {
     let d = {'asc': +1, 'desc': -1};
     return d[ds[0]] * (a[fs[0]] > b[fs[0]] ? +1 : -1);
 }
+const cloneDeep = (obj) => {
+    let result = {};
+    for (let key in obj) {
+        if (typeof(obj[key]) === 'object') {
+            result[key] = cloneDeep(obj[key]);
+        }
+        else result[key] = obj[key];
+    }
+    return result;
+}
+
 
 
 module.exports = {
     about,
     get_files_filter,
-    comparator
+    comparator,
+    cloneDeep
 }
