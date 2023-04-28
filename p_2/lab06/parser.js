cc4ebe37bf8741f8cfd607d1b72c9fcd7eb9192b
Sonechek <flex.tlex@gmail.com>
2023-04-28 14:31:54 +0500

    main

--- p_2/lab06/parser.js
+++
@@ -1,9 +0,0 @@
-const sqlite = require('sqlite-sync')
-sqlite.connect('prods.db')
-
-const change = str => str.replace(/[\s|P]+ /g, '')
-
-sqlite.create_function(change)
-
-let sql_select =
-    SELECT title, change(price)
