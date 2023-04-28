f1424afc4be4ef8ad2fad16f81757bda62b234ba
Sonechek <flex.tlex@gmail.com>
2023-04-28 16:18:05 +0500

    поправить автоинкремент

--- p_2/lab06/2.js
+++ p_2/lab06/2.js
@@ -1,19 +1,30 @@
 const sqlite = require('sqlite-sync')
 
-sqlite.connect('prods.db')
+sqlite.connect('./prods.db')
 
 sql_create = `
     CREATE TABLE IF NOT EXISTS prods(
         id INTEGER UNIQUE,
         title TEXT,
         href TEXT,
-        price INTEGER,
-        stores TEXT,
+        priceDown INTEGER,
+        priceUp INTEGER,
         PRIMARY KEY (id AUTOINCREMENT)
     );
 `
 
 sqlite.run(sql_create, (res) => {
+    if (res.error) throw res.error;
+})
+let prods = require('./result.json')
+
+let recs = prods
+    .map(p => `('${p.id}','${p.title}','${p.href}','${p.priceDown}','${p.priceUp}')`)
+
+let sql_insert = `INSERT INTO prods VALUES ${recs.join(',')}`
+
+console.log(sql_insert)
+sqlite.run(sql_insert, (res) => {
     if (res.error) throw res.error;
 })
 
